4d418d3c4c7522fe815ce240c72138ce20df4cfc
Isaac Williams <Isaaciyaye174@gmail.com>
2024-08-20 12:52:37 +0100

    Update SelectBalance.tsx

--- app/_components/premitives/modals/select-balance/SelectBalance.tsx
+++ app/_components/premitives/modals/select-balance/SelectBalance.tsx
@@ -53,9 +53,11 @@ const SelectBalance = ({ data }: Props) => {
         </div>
       </div>
       <div className="select_balance_texts">
-        {/* <div className="confirm_bet_coupon_text1">
-          You have an active casino bonus
-        </div> */}
+        {user?.casinoBonusBalance > 0 && (
+          <div className="confirm_bet_coupon_text1">
+            You have an active casino bonus
+          </div>
+        )}
         <div className="confirm_bet_coupon_text1">
           Choose the balance to use for this game sessions
         </div>
